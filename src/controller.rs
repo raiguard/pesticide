6361f719a2bedbb54415a6c97291253ce59eb6c0
raiguard <caleb.heuer@gmail.com>
2022-04-30 23:07:30 -0600

    Split handlers into separate functions

--- src/controller.rs
+++ src/controller.rs
@@ -14,171 +14,9 @@ pub fn start(adapter: Arc<Mutex<Adapter>>) -> Result<()> {
         for msg in rx {
             let mut adapter = event_adapter.lock().unwrap();
             match msg {
-                AdapterMessage::Event(event) => match event {
-                    Event::Exited(_) => handle_exited(&mut adapter),
-                    Event::Output(payload) => {
-                        trace!("Updating seq");
-                        adapter.update_seq(payload.seq);
-                        trace!("Updated seq");
-                        if let Some(body) = payload.body {
-                            match body.category {
-                                Some(OutputEventCategory::Telemetry) => {
-                                    info!("IDGAF about telemetry")
-                                } // IDGAF about telemetry
-                                _ => info!("[DEBUG ADAPTER] >> {}", body.output),
-                            }
-                        }
-                    }
-                    Event::Initialized(_) => {
-                        info!("Debug adapter is initialized");
-                        // TODO: setBreakpoints, etc...
-                        let req =
-                            AdapterMessage::Request(Request::ConfigurationDone(RequestPayload {
-                                seq: adapter.next_seq(),
-                                args: None,
-                            }));
-
-                        adapter.tx.send(req).unwrap();
-                    }
-                    Event::Process(_) => (), // TODO: What is this event useful for?
-                    Event::Stopped(event) => {
-                        if let Some(body) = event.body {
-                            println!("STOPPED on thread {}: {:?}", body.thread_id, body.reason);
-                        }
-
-                        // Request threads
-                        let req = AdapterMessage::Request(Request::Threads(RequestPayload {
-                            seq: adapter.next_seq(),
-                            args: None,
-                        }));
-                        adapter.tx.send(req).unwrap();
-                    }
-                    Event::Thread(event) => {
-                        if let Some(body) = event.body {
-                            info!("New thread started: {}", body.thread_id);
-                            match body.reason {
-                                ThreadReason::Started => {
-                                    adapter.threads.insert(
-                                        body.thread_id,
-                                        Thread {
-                                            id: body.thread_id,
-                                            // This will be replaced with the actual names in the Threads request
-                                            name: format!("{}", body.thread_id),
-                                        },
-                                    );
-                                }
-                                ThreadReason::Exited => {
-                                    if adapter.threads.remove(&body.thread_id).is_none() {
-                                        error!(
-                                            "Thread {} ended, but had no stored data",
-                                            body.thread_id
-                                        )
-                                    }
-                                }
-                            };
-                        }
-                    }
-                },
-                AdapterMessage::Request(req) => {
-                    // The only "reverse request" in the DAP is RunInTerminal
-                    if let Request::RunInTerminal(req) = req {
-                        if let Some(mut args) = req.args {
-                            let mut term_cmd = adapter.config.term_cmd.clone();
-                            term_cmd.append(&mut args.args);
-
-                            let cmd = Command::new(term_cmd[0].clone())
-                                .args(term_cmd[1..].to_vec())
-                                .spawn();
-
-                            let (success, message) = match &cmd {
-                                Ok(_) => (true, None),
-                                Err(e) => {
-                                    error!("Could not start debugee: {}", e);
-                                    (false, Some(e.to_string()))
-                                }
-                            };
-
-                            let mut adapter = adapter;
-                            let res = AdapterMessage::Response(Response::RunInTerminal(
-                                ResponsePayload {
-                                    seq: adapter.next_seq(),
-                                    request_seq: req.seq,
-                                    success,
-                                    message,
-                                    body: Some(RunInTerminalResponse {
-                                        process_id: cmd.ok().map(|child| child.id()),
-                                        shell_process_id: None, // TEMPORARY:
-                                    }),
-                                },
-                            ));
-
-                            adapter.tx.send(res).unwrap();
-                        }
-                    }
-                }
-                AdapterMessage::Response(res) => match res {
-                    Response::ConfigurationDone(_) => (),
-                    Response::Initialize(res) => {
-                        // Save capabilities to Adapter
-                        let mut adapter = adapter;
-                        adapter.capabilities = res.body;
-
-                        // Send launch request
-                        // This differs from how the DAP event order is specified on the DAP website
-                        // See https://github.com/microsoft/vscode/issues/4902#issuecomment-368583522
-                        let seq = adapter.next_seq();
-                        adapter
-                            .tx
-                            .send(AdapterMessage::Request(Request::Launch(RequestPayload {
-                                args: Some(adapter.config.launch_args.clone()),
-                                seq,
-                            })))
-                            .unwrap();
-                    }
-                    Response::Launch(res) => {
-                        if res.success {
-                        } else {
-                            error!(
-                                "Could not launch debug adapter: {}",
-                                res.message.unwrap_or_default()
-                            );
-                        }
-                    }
-                    Response::RunInTerminal(_) => (),
-                    Response::StackTrace(res) => {
-                        if let Some(body) = res.body {
-                            println!("{:#?}", body);
-                        }
-                    }
-                    Response::StepIn(_) => (),
-                    Response::Threads(res) => {
-                        if let Some(body) = res.body {
-                            // Update the stored threads
-                            let threads = &body.threads;
-                            adapter.threads = threads
-                                .iter()
-                                .cloned()
-                                .map(|thread| (thread.id, thread))
-                                .collect();
-
-                            // Request stack frames for each thread
-                            for thread in threads {
-                                let req =
-                                    AdapterMessage::Request(Request::StackTrace(RequestPayload {
-                                        seq: adapter.next_seq(),
-                                        args: Some(StackTraceRequest {
-                                            thread_id: thread.id,
-                                            start_frame: None,
-                                            levels: None,
-                                            format: None,
-                                        }),
-                                    }));
-
-                                adapter.tx.send(req).unwrap();
-                            }
-                        }
-                    }
-                },
+                AdapterMessage::Event(event) => handle_event(&mut adapter, event),
+                AdapterMessage::Request(req) => handle_request(&mut adapter, req),
+                AdapterMessage::Response(res) => handle_response(&mut adapter, res),
             }
         }
     });
@@ -226,6 +64,7 @@ pub fn start(adapter: Arc<Mutex<Adapter>>) -> Result<()> {
     });
 
     {
+        // This will be dropped at the end of this inner scope, freeing it up
         let adapter = adapter.lock().unwrap();
 
         // Send initialize request
@@ -263,4 +102,169 @@ fn handle_exited(adapter: &mut MutexGuard<Adapter>) {
         error!("Failed to kill debug adapter: {}", e)
     }
     // Pesticide will exit due to the debug adapter pipe closing
+}
+
+fn handle_event(adapter: &mut MutexGuard<Adapter>, event: Event) {
+    match event {
+        Event::Exited(_) => handle_exited(adapter),
+        Event::Output(payload) => {
+            trace!("Updating seq");
+            adapter.update_seq(payload.seq);
+            trace!("Updated seq");
+            if let Some(body) = payload.body {
+                match body.category {
+                    Some(OutputEventCategory::Telemetry) => {
+                        info!("IDGAF about telemetry")
+                    } // IDGAF about telemetry
+                    _ => info!("[DEBUG ADAPTER] >> {}", body.output),
+                }
+            }
+        }
+        Event::Initialized(_) => {
+            info!("Debug adapter is initialized");
+            // TODO: setBreakpoints, etc...
+            let req = AdapterMessage::Request(Request::ConfigurationDone(RequestPayload {
+                seq: adapter.next_seq(),
+                args: None,
+            }));
+
+            adapter.tx.send(req).unwrap();
+        }
+        Event::Process(_) => (), // TODO: What is this event useful for?
+        Event::Stopped(event) => {
+            if let Some(body) = event.body {
+                println!("STOPPED on thread {}: {:?}", body.thread_id, body.reason);
+            }
+
+            // Request threads
+            let req = AdapterMessage::Request(Request::Threads(RequestPayload {
+                seq: adapter.next_seq(),
+                args: None,
+            }));
+            adapter.tx.send(req).unwrap();
+        }
+        Event::Thread(event) => {
+            if let Some(body) = event.body {
+                info!("New thread started: {}", body.thread_id);
+                match body.reason {
+                    ThreadReason::Started => {
+                        adapter.threads.insert(
+                            body.thread_id,
+                            Thread {
+                                id: body.thread_id,
+                                // This will be replaced with the actual names in the Threads request
+                                name: format!("{}", body.thread_id),
+                            },
+                        );
+                    }
+                    ThreadReason::Exited => {
+                        if adapter.threads.remove(&body.thread_id).is_none() {
+                            error!("Thread {} ended, but had no stored data", body.thread_id)
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
+
+fn handle_request(adapter: &mut MutexGuard<Adapter>, req: Request) {
+    {
+        // The only "reverse request" in the DAP is RunInTerminal
+        if let Request::RunInTerminal(req) = req {
+            if let Some(mut args) = req.args {
+                let mut term_cmd = adapter.config.term_cmd.clone();
+                term_cmd.append(&mut args.args);
+
+                let cmd = Command::new(term_cmd[0].clone())
+                    .args(term_cmd[1..].to_vec())
+                    .spawn();
+
+                let (success, message) = match &cmd {
+                    Ok(_) => (true, None),
+                    Err(e) => {
+                        error!("Could not start debugee: {}", e);
+                        (false, Some(e.to_string()))
+                    }
+                };
+
+                let res = AdapterMessage::Response(Response::RunInTerminal(ResponsePayload {
+                    seq: adapter.next_seq(),
+                    request_seq: req.seq,
+                    success,
+                    message,
+                    body: Some(RunInTerminalResponse {
+                        process_id: cmd.ok().map(|child| child.id()),
+                        shell_process_id: None, // TEMPORARY:
+                    }),
+                }));
+
+                adapter.tx.send(res).unwrap();
+            }
+        }
+    }
+}
+
+fn handle_response(adapter: &mut MutexGuard<Adapter>, res: Response) {
+    match res {
+        Response::ConfigurationDone(_) => (),
+        Response::Initialize(res) => {
+            // Save capabilities to Adapter
+            adapter.capabilities = res.body;
+
+            // Send launch request
+            // This differs from how the DAP event order is specified on the DAP website
+            // See https://github.com/microsoft/vscode/issues/4902#issuecomment-368583522
+            let seq = adapter.next_seq();
+            adapter
+                .tx
+                .send(AdapterMessage::Request(Request::Launch(RequestPayload {
+                    args: Some(adapter.config.launch_args.clone()),
+                    seq,
+                })))
+                .unwrap();
+        }
+        Response::Launch(res) => {
+            if res.success {
+            } else {
+                error!(
+                    "Could not launch debug adapter: {}",
+                    res.message.unwrap_or_default()
+                );
+            }
+        }
+        Response::RunInTerminal(_) => (),
+        Response::StackTrace(res) => {
+            if let Some(body) = res.body {
+                println!("{:#?}", body);
+            }
+        }
+        Response::StepIn(_) => (),
+        Response::Threads(res) => {
+            if let Some(body) = res.body {
+                // Update the stored threads
+                let threads = &body.threads;
+                adapter.threads = threads
+                    .iter()
+                    .cloned()
+                    .map(|thread| (thread.id, thread))
+                    .collect();
+
+                // Request stack frames for each thread
+                for thread in threads {
+                    let req = AdapterMessage::Request(Request::StackTrace(RequestPayload {
+                        seq: adapter.next_seq(),
+                        args: Some(StackTraceRequest {
+                            thread_id: thread.id,
+                            start_frame: None,
+                            levels: None,
+                            format: None,
+                        }),
+                    }));
+
+                    adapter.tx.send(req).unwrap();
+                }
+            }
+        }
+    }
 }
