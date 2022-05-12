87632ba1e237b064758d194e4180c4b7a85704fb
raiguard <caleb.heuer@gmail.com>
2022-05-12 16:47:05 -0600

    Set up basic crossterm input handling

--- src/client.rs
+++ src/client.rs
@@ -1,25 +1,24 @@
 use anyhow::Result;
+use crossterm::event::KeyCode;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use futures_util::{SinkExt, StreamExt};
 use std::path::PathBuf;
-use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::UnixStream;
 use tokio::select;
 use tokio_util::codec::{Framed, LinesCodec};
 
 pub async fn run(socket_path: PathBuf) -> Result<()> {
+    // Set up terminal
+    enable_raw_mode()?;
+
     let socket = UnixStream::connect(socket_path).await.unwrap();
     let mut socket = Framed::new(socket, LinesCodec::new());
-    let mut stdin = BufReader::new(tokio::io::stdin());
+    let mut input_stream = crossterm::event::EventStream::new();
 
     loop {
-        trace!("New loop");
-        let mut input = String::new();
         select! {
             // User input
-            Ok(_) = stdin.read_line(&mut input) => {
-                let input = input + "\n";
-                socket.send(input).await?;
-            },
+            Some(Ok(event)) = input_stream.next() => handle_input(&mut socket, event).await?,
             // Messages from server
             msg = socket.next() => {
                 match msg {
@@ -30,7 +29,7 @@ pub async fn run(socket_path: PathBuf) -> Result<()> {
                             "quit" => break,
                             _ => ()
                         }
-                    },
+                    }
                     Some(Err(e)) => {
                         error!("Socket error: {e}");
                     }
@@ -43,6 +42,45 @@ pub async fn run(socket_path: PathBuf) -> Result<()> {
         }
     }
 
-    trace!("End of loop");
+    // Restore terminal
+    disable_raw_mode()?;
+
+    Ok(())
+}
+
+async fn handle_input(
+    socket: &mut Framed<UnixStream, LinesCodec>,
+    event: crossterm::event::Event,
+) -> Result<()> {
+    match event {
+        crossterm::event::Event::Key(event) => match event.code {
+            KeyCode::Backspace => (),
+            KeyCode::Enter => (),
+            KeyCode::Left => (),
+            KeyCode::Right => (),
+            KeyCode::Up => (),
+            KeyCode::Down => (),
+            KeyCode::Home => (),
+            KeyCode::End => (),
+            KeyCode::PageUp => (),
+            KeyCode::PageDown => (),
+            KeyCode::Tab => (),
+            KeyCode::BackTab => (),
+            KeyCode::Delete => (),
+            KeyCode::Insert => (),
+            KeyCode::F(_) => (),
+            KeyCode::Char(c) => match c {
+                'q' => socket.send("quit".to_string()).await?,
+                'i' => socket.send("in".to_string()).await?,
+                _ => (),
+            },
+            KeyCode::Null => (),
+            KeyCode::Esc => (),
+        },
+        crossterm::event::Event::Mouse(_) => (),
+        crossterm::event::Event::Resize(_, _) => (),
+    };
+    println!("{:?}", event);
+
     Ok(())
 }
