b9b8498451204ff649713de98946a61a258e7e21
raiguard <caleb.heuer@gmail.com>
2022-05-12 17:39:35 -0600

    Draw dummy UI

--- src/client.rs
+++ src/client.rs
@@ -1,20 +1,35 @@
 use anyhow::Result;
-use crossterm::event::KeyCode;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
 use futures_util::{SinkExt, StreamExt};
+use std::io::Stdout;
 use std::path::PathBuf;
 use tokio::net::UnixStream;
 use tokio::select;
 use tokio_util::codec::{Framed, LinesCodec};
+use tui::backend::CrosstermBackend;
+use tui::{widgets, Terminal};
 
 pub async fn run(socket_path: PathBuf) -> Result<()> {
-    // Set up terminal
+    // Prepare terminal
     enable_raw_mode()?;
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
+    // Set up server and input comms
     let socket = UnixStream::connect(socket_path).await.unwrap();
     let mut socket = Framed::new(socket, LinesCodec::new());
     let mut input_stream = crossterm::event::EventStream::new();
 
+    // Draw UI with initial state
+    draw_ui(&mut terminal)?;
+
+    // Main loop
     loop {
         select! {
             // User input
@@ -40,10 +55,17 @@ pub async fn run(socket_path: PathBuf) -> Result<()> {
                 }
             }
         }
+        draw_ui(&mut terminal)?
     }
 
     // Restore terminal
     disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
     Ok(())
 }
@@ -70,7 +92,11 @@ async fn handle_input(
             KeyCode::Insert => (),
             KeyCode::F(_) => (),
             KeyCode::Char(c) => match c {
-                'q' => socket.send("quit".to_string()).await?,
+                // Quit client
+                // TEMPORARY:
+                'q' => std::process::exit(0),
+                // // Kill session
+                // 'k' => socket.send("quit".to_string()).await?,
                 'i' => socket.send("in".to_string()).await?,
                 _ => (),
             },
@@ -80,7 +106,17 @@ async fn handle_input(
         crossterm::event::Event::Mouse(_) => (),
         crossterm::event::Event::Resize(_, _) => (),
     };
-    println!("{:?}", event);
+
+    Ok(())
+}
+
+fn draw_ui(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
+    terminal.draw(|f| {
+        let block = widgets::Block::default()
+            .title("Pesticide")
+            .borders(widgets::Borders::ALL);
+        f.render_widget(block, f.size());
+    })?;
 
     Ok(())
 }
