d9c7e8ceb7bf152e6a9aae9d7a30b2d286afa1c6
raiguard <caleb.heuer@gmail.com>
2022-05-11 22:41:49 -0600

    Massively simplify client logic

--- src/client.rs
+++ src/client.rs
@@ -1,69 +1,47 @@
-use crate::dap_types::*;
 use anyhow::Result;
+use futures_util::{SinkExt, StreamExt};
 use std::path::PathBuf;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::UnixStream;
 use tokio::select;
-use tokio::sync::{broadcast, mpsc};
+use tokio_util::codec::{Framed, LinesCodec};
 
 pub async fn run(socket_path: PathBuf) -> Result<()> {
-    // Server message handling task
-    let (to_server_tx, mut to_server_rx) = mpsc::channel::<String>(32);
-    let (server_in_tx, mut server_in_rx) = broadcast::channel::<String>(32);
-    tokio::spawn(async move {
-        let server = UnixStream::connect(socket_path).await.unwrap();
-        let (server_rd, mut server_wr) = tokio::io::split(server);
-        let mut server_rd = BufReader::new(server_rd);
-        let mut read_buf = String::new();
-
-        loop {
-            select! {
-                res = to_server_rx.recv() => {
-                    match res {
-                        Some(mut req) => {
-                            req += "\n";
-                            server_wr.write_all(req.as_bytes()).await.unwrap();
-                        },
-                        None => break,
-
-                    };
-                }
-                res = server_rd.read_line(&mut read_buf) => {
-                    match res {
-                        Ok(0) => {
-                            println!("Server disconnected");
-                            server_in_tx.send("quit".to_string()).unwrap();
-                            break
-                        },
-                        Ok(_) => {
-                            let msg = read_buf.trim();
-                            println!("FROM SERVER: {}", msg);
-                            server_in_tx.send(msg.to_string()).unwrap();
-                        },
-                        Err(e) => eprintln!("{}", e),
-                    };
-                    read_buf.clear();
-                }
-            }
-        }
-    });
-
+    let socket = UnixStream::connect(socket_path).await.unwrap();
+    let mut socket = Framed::new(socket, LinesCodec::new());
     let mut stdin = BufReader::new(tokio::io::stdin());
+
     loop {
+        trace!("New loop");
         let mut input = String::new();
         select! {
+            // User input
             Ok(_) = stdin.read_line(&mut input) => {
-                to_server_tx.send(input).await?;
+                let input = input + "\n";
+                socket.send(input).await?;
             },
-            Ok(msg) = server_in_rx.recv() => {
-                #[allow(clippy::single_match)]
-                match msg.as_str() {
-                    "quit" => break,
-                    _ => ()
+            // Messages from server
+            msg = socket.next() => {
+                match msg {
+                    Some(Ok(msg)) => {
+                        #[allow(clippy::single_match)]
+                        match msg.as_str() {
+                            "quit" => break,
+                            _ => ()
+                        }
+                    },
+                    Some(Err(e)) => {
+                        error!("Socket error: {e}");
+                    }
+                    None => {
+                        info!("Server disconnected");
+                        break
+                    }
                 }
             }
         }
     }
 
+    trace!("End of loop");
     Ok(())
 }
